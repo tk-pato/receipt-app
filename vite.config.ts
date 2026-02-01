a603f4000e9ca4fcef0d7435969249ac427e970f
tk-pato <tk.patolaqshe@gmail.com>
2026-02-01 16:02:24 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // ▼▼▼ 重要: GitHub Pagesで画面が真っ白になるのを防ぐ設定 ▼▼▼
+  // ▼▼▼ 重要: GitHub Pages用の設定 ▼▼▼
   base: './', 
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
   },
   server: {
     host: true,
